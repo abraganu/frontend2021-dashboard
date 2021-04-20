eafcdb4c570d391c0560199bc51e203fbaadaa3f
samuel <samuel.martinez.morales@gmail.com>
2021-04-19 20:25:13 -0500

    todo modal to create new task

--- todos.js
+++ todos.js
@@ -2,6 +2,10 @@
 const tableBody = document.getElementById('todosTableBody');
 const usersOptionBtn = document.getElementById('users-tab');
 const ordersOptionBtn = document.getElementById('orders-tab');
+const createTodoForm = document.getElementById('createTodoForm');
+const createTodoModal = new bootstrap.Modal(document.getElementById('createTodoModal'), {
+    keyboard: false
+});
 
 
 window.addEventListener('load', async function() {
@@ -18,6 +22,30 @@ ordersOptionBtn.addEventListener('click', function() {
     window.location.href = 'http://127.0.0.1:5500/orders.html';
 });
 
+
+
+createOrderForm.addEventListener("submit", async function(event){
+    event.preventDefault();
+    const [userId, title, completed] = event.target;
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        body: JSON.stringify({
+            userId: userId.value,
+            title: title.value,
+            completed: completed.value
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    const createdTodo = await response.json();
+    createTodoModal.hide();
+    createTableRow(createdTodo);
+})
+
+
+
+
 function createTableRow(todo) {
     const tableRow = `
         <tr>
@@ -29,3 +57,4 @@ function createTableRow(todo) {
     `
     tableBody.insertAdjacentHTML('beforeend', tableRow)
 }
+
