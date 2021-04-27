5088155ba11a37f35ed82e5bf037c2ff62fffde8
anunez <abraham.garcia@encora.com>
2021-04-26 20:47:18 -0500

    Agregar scripts de ejemplo

---
+++ scripts.js
@@ -0,0 +1,54 @@
+let people = [];
+let films = [];
+let species = [];
+let starships = [];
+let vehicles = [];
+let mappedPeople = [];
+
+function mappedPeopleData(people = []) {
+    return people.map((person) => {
+        films = films.concat(person.films);
+        starships = starships.concat(person.starships);
+        return {
+            birthYear: person.birth_year,
+            created: person.created,
+            edited: person.edited,
+            eyeColor: person.eye_color,
+            gender: person.gender,
+            hairColor: person.hair_color,
+            height: person.height,
+            homeWorld: person.homeworld,
+            mass: person.mass,
+            name: person.name,
+            skinColor: person.skin_color,
+            url: person.url
+        }
+    })
+}
+
+async function getPeople() {
+    const response = await fetch('https://swapi.dev/api/people');
+    const data = await response.json();
+    return mappedPeopleData(data.results);
+}
+
+getPeople().then((people) => {
+    const foundItems = people.filter((person) => {
+        return person.mass === '32';
+    })
+    console.log(starships);
+    console.log(films);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
